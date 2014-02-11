1b29c2d97a324ec70df34b68102672c1b57426d2
Xuejie "Rafael" Xiao <xxuejie@gmail.com>
2014-02-11 20:24:48 +0800

    When fetching data via TcpReceive, do not check if the socket is closed. This is because HTTP server would close the connection as soon as the data are sent(in HTTP 1.0 mode).

--- E2ForLife_W5100.cylib/E2ForLife_W5100_v1_1/API/W5100.c
+++ E2ForLife_W5100.cylib/E2ForLife_W5100_v1_1/API/W5100.c
@@ -1445,31 +1445,29 @@ uint16
 `$INSTANCE_NAME`_TcpReceive( uint8 socket, uint8* buffer, uint16 length )
 {
 	uint16 RxSize;
-	
+
 	/*
 	 * First thing to do is to check to see if the socket is connected
 	 * and that there is data waiting
 	 */
 	RxSize = 0;
-	if (`$INSTANCE_NAME`_GetSocketStatus( socket ) == 0x17) {
-		/*
-		 * read the number of waiting bytes in the buffer memory
-		 * but, clip the length of data read to the requested
-		 * length of data.
-		 */
-		RxSize = `$INSTANCE_NAME`_GetRxSize( socket );
-		RxSize = (RxSize > length) ? length : RxSize;
-		/* If there was waiting data, read it from the buffer */
-		if (RxSize > 0) {
-			`$INSTANCE_NAME`_ProcessRxData( socket, 0, buffer, RxSize, 0);
-			/* 
-			 * after reading the buffer data, send the receive command
-			 * to the socket so that the W5100 completes the read
-			 */
-			`$INSTANCE_NAME`_ExecuteSocketCommand(socket, 0x40);
-		}
-	}
-	
+  /*
+   * read the number of waiting bytes in the buffer memory
+   * but, clip the length of data read to the requested
+   * length of data.
+   */
+  RxSize = `$INSTANCE_NAME`_GetRxSize( socket );
+  RxSize = (RxSize > length) ? length : RxSize;
+  /* If there was waiting data, read it from the buffer */
+  if (RxSize > 0) {
+    `$INSTANCE_NAME`_ProcessRxData( socket, 0, buffer, RxSize, 0);
+    /* 
+     * after reading the buffer data, send the receive command
+     * to the socket so that the W5100 completes the read
+     */
+    `$INSTANCE_NAME`_ExecuteSocketCommand(socket, 0x40);
+  }
+
 	/* return the number of read bytes from the buffer memory */
 	return RxSize;
 }
